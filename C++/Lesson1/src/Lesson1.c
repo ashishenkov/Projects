36eb8c7d8071bafa90fbffbd4d3b5d4c51f455a3
Alexandr <ashishenkov@gmail.com>
2018-03-12 12:48:45 +0300

    Create new education lesson

---
+++ C++/Lesson1/src/Lesson1.c
@@ -0,0 +1,16 @@
+/*
+ ============================================================================
+ Name        : Lesson1.c
+ Author      : Alexandr Shishenkov
+ Version     : 0.1
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return 0;
+}

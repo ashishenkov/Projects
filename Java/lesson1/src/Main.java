b1b45a152480861b2e80a35b78dee49d433a6f0b
Alexandr <ashishenkov@gmail.com>
2018-03-24 12:31:15 +0300

    Выполнения 2 задания

--- Java/lesson1/src/Main.java
+++ Java/lesson1/src/Main.java
@@ -6,6 +6,7 @@ import java.math.*;
  * Java. Lesson 1. Homework
  * 1 Ввести вес и рост человека. Рассчитать и вывести индекс массы тела по формуле I=m/(h*h);
  * где m-масса тела в килограммах, h - рост в метрах.
+ * 2 Найти максимальное из четырех чисел. Массивы не использовать.
  *
  * @author Alexandr Shishenkov
  * @version March 22, 2018
@@ -14,12 +15,34 @@ public class Main {
 
     public static void main(String[] args) {
 
-    bodyIndex();
+    //bodyIndex();
+        maxNumber();
+    }
+    static void maxNumber() {
+        int max = 0;
+        int num1 = 0;
+        int i=0;
+        do {
+
+            System.out.println("Input number: ");
+            Scanner scanner = new Scanner(System.in);
+            int num = scanner.nextInt();
+            //num1 =  num;
+            if (num1 > num){
+                num1 = max;
+
+            }else if(num1 < num){
+                num1 = num;
+            }
+            max = num1;
+            i++;
+        }while (i != 4);
+        System.out.println("Maximum number: " + max);
 
     }
 
 
-public static void bodyIndex() {
+    public static void bodyIndex() {
     DecimalFormat df = new DecimalFormat("#.##");
     df.setRoundingMode(RoundingMode.CEILING);
     System.out.println("Input man's weight in kg : ");

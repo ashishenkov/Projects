dce915f9e2249f1cc706f90e0334dd621488308c
Alexandr <ashishenkov@gmail.com>
2018-04-02 18:19:54 +0300

    создание папки для упражнений

---
+++ Java/JavaExercises/ReadCharRunner.java
@@ -0,0 +1,13 @@
+public class ReadCharRunner{
+	public static void main(String[] args){
+			int x;
+			try{
+				x = System.in.read();
+				char c = (char) x;
+				System.out.println( "Код символа" + c "= " + x);
+			}catch (java.io.IOException e){
+				System.err.println("Ошибка ввода " + e);
+			}
+	}
+	
+}

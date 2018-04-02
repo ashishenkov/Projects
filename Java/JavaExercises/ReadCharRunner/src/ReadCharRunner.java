b48446c50916a0674fd5d050ceb3cc4d8c14c327
Alexandr <ashishenkov@gmail.com>
2018-04-02 18:37:04 +0300

    и

--- Java/JavaExercises/ReadCharRunner/src/ReadCharRunner.java
+++ Java/JavaExercises/ReadCharRunner/src/ReadCharRunner.java
@@ -1,12 +1,13 @@
-public class ReadCharRunner{
-    public static void main(String[] args){
+public class ReadCharRunner {
+    public static void main(String[] args) {
         int x;
-        try{
+        try {
             System.out.println("Введите символ: ");
             x = System.in.read();
             char c = (char) x;
-            System.out.println( "Код символа " + c + "= " + x);
-        }catch (java.io.IOException e){
+            System.out.println("Код символа " + c + "= " + x);
+        } catch (java.io.IOException e) {
             System.err.println("Ошибка ввода " + e);
         }
     }
+}

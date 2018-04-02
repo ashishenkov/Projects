64d122bb60394349eb3b824c4996588a44ed9786
Alexandr <ashishenkov@gmail.com>
2018-04-02 18:36:36 +0300

    упражнение

---
+++ Java/JavaExercises/ReadCharRunner/src/ReadCharRunner.java
@@ -0,0 +1,12 @@
+public class ReadCharRunner{
+    public static void main(String[] args){
+        int x;
+        try{
+            System.out.println("Введите символ: ");
+            x = System.in.read();
+            char c = (char) x;
+            System.out.println( "Код символа " + c + "= " + x);
+        }catch (java.io.IOException e){
+            System.err.println("Ошибка ввода " + e);
+        }
+    }
